aaccecaa528a6fe80644b38621b575f224271ddd
Kalugin68 <sashakalugin74@gmail.com>
2026-06-19 09:51:33 +0300

    fix CI/CD

--- app/services/db_init_service.py
+++ app/services/db_init_service.py
@@ -13,7 +13,7 @@ class DBInitService:
                 CREATE TABLE IF NOT EXISTS users
                 (
                     id SERIAL PRIMARY KEY,
-                    login VARCHAR(255) NOT NULL,
+                    login VARCHAR(255) UNIQUE NOT NULL,
                     password VARCHAR(255) NOT NULL,
                     name VARCHAR(255) NOT NULL
                 )
