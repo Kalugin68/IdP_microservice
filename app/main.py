20e6769b79e9dae489aa79102e7750bce75d912a
Kalugin68 <sashakalugin74@gmail.com>
2026-06-23 14:55:59 +0300

    Fix CI/CD

--- app/main.py
+++ app/main.py
@@ -2,19 +2,14 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from services.oauth_service import OauthService
-from services.token_service import Token
 from services.clients_service import ClientService
-from config import Env
 from api import api_routes
 from jat import TokenManager
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    env = Env()
-
     app.state.token_manager = TokenManager()
-    app.state.token_service = Token(secret_key=env.jwt_secret)
     app.state.oauth_service = OauthService()
     app.state.clients_service = ClientService()
 
